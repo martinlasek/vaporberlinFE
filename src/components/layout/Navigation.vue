f029251d9b751f6a5ced0ff3ace973a40a831bcf
Martin Lasek <martinlasek@hotmail.de>
2017-09-05 22:18:48 +0200

    styled navigation / small improvements on box sizes

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -17,43 +17,5 @@
 </script>
 
 <style lang="less">
-  nav {
-    display: block;
-    margin-bottom: 30px;
-    min-height: 50px;
-
-    .logo {
-      padding: 5px 0;
-      text-align: center;
-      width: 100%;
-
-      a {
-        font-weight: 400;
-        &:hover {
-          border-bottom: 1px solid transparent;
-        }
-      }
-    }
-
-    .menu {
-      align-content: center;
-      display: flex;
-      justify-content: space-around;
-      width: 100%;
-    }
-
-    a {
-      border-bottom: 1px solid transparent;
-      color: #ffffff;
-      display: inline-block;
-      font-size: 22px;
-      font-weight: 100;
-      text-decoration: none;
-      padding: 5px 10px;
-
-      &:hover {
-         border-bottom: 1px solid #ffffff;
-       }
-    }
-  }
+  @import "../../assets/styles/css/min/Navigation.less";
 </style>
