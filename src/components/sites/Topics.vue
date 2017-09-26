c783b9469fb189e9f70c03f4eb2c54f897333f1e
Martin Lasek <martinlasek@hotmail.de>
2017-09-26 19:36:25 +0200

    added + styled button to create a new topic

--- src/components/sites/Topics.vue
+++ src/components/sites/Topics.vue
@@ -4,6 +4,13 @@
     <template slot="content">
 
       <page-header> TOPICS </page-header>
+      <div class="create-topic">
+        <div class="row">
+          <div class="col-xs-12">
+            <submit-button text="create new topic" toEmit="clicked" @clicked="create"/>
+          </div>
+        </div>
+      </div>
       <div v-for="topic in list" class="panel panel-default">
         <div class="panel-body">
           {{ topic.description }}
@@ -18,14 +25,21 @@
   import StandardLayout from '../layout/Standard.vue'
   import PageHeader from '../elements/PageHeader.vue'
   import {fetchTopicList} from '../../api/api'
+  import SubmitButton from '../elements/SubmitButton.vue'
 
   export default {
 
-    components: { StandardLayout, PageHeader },
+    components: { StandardLayout, PageHeader, SubmitButton },
 
     computed: {
       list() {
         return this.$store.getters.getTopicList;
+      }
+    },
+
+    methods: {
+      create() {
+        console.log("create");
       }
     },
 
@@ -34,3 +48,7 @@
     }
   }
 </script>
+
+<style lang="less">
+  @import "../../assets/styles/css/min/Topics.less";
+</style>
